36ab4d85e60daca518848339f6579368e57895aa
Kedar Iyer <kedarmail@gmail.com>
2018-05-25 16:20:13 -0700

    voting on jungle testnet with basic styles

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ getProducers().then(checkLibertyBlock);
 
 function checkLibertyBlock() {
     document.querySelector("input[value=libertylion1]").checked = true
-    updateBPList();
+    updateSelectedBPs();
+}
+
+function filterProds () {
+    var search = document.getElementById('filter-prods').value;
+    document.querySelectorAll('.prod-row').forEach(function (row) {
+        if (search === "")
+            row.style.display = "table-row";
+        else if (row.childNodes[3].textContent.indexOf(search) > -1)
+            row.style.display = "table-row";
+        else
+            row.style.display = "none";
+    });
 }
 
 function getEos() {
@@ -26,9 +38,10 @@ function getProducers() {
         limit: 200
     }
     var tbody = document.querySelector("#block-producers tbody");
+
     return eos.getTableRows(params).then(resp => {
         var sorted = resp.rows.sort((a,b) => Number(a.total_votes) > Number(b.total_votes) ? -1:1);
-        sorted.map(prod => `<tr>
+        sorted.map(prod => `<tr class="prod-row">
             <td><input type="checkbox" name="vote-prods" value="${prod.owner}"></td>
             <td>${prod.owner}</td>
             <td>${prettyNumber(prod.total_votes)}</td>
@@ -36,20 +49,25 @@ function getProducers() {
         .forEach(row => tbody.innerHTML += row);
 
         document.getElementsByName('vote-prods').forEach(e => {
-            e.onclick = updateBPList;
+            e.onclick = updateSelectedBPs;
         });
 
     });
 
 }
 
-function updateBPList() {
+function getSelectedBPs () {
     var checked = []
     document.getElementsByName('vote-prods').forEach(function (prod) {
         if (prod.checked)
             checked.push(prod.value);
     });
-    document.getElementById("my-bps").innerHTML = checked.join(', ');
+    return checked;
+}
+
+function updateSelectedBPs() {
+    var checked = getSelectedBPs();
+    document.getElementById("selected-bps").innerHTML = checked.join(', ');
 }
 
 function prettyNumber(num) {
@@ -59,8 +77,27 @@ function prettyNumber(num) {
 
 function vote () {
     var eos = getEos();
+    var selectedBPs = getSelectedBPs();
+    if (selectedBPs.length > 30) {
+        var alert = `<div class="alert alert-danger" role="alert">
+            Maximum 30 block producers can be selected
+        </div>`
+        document.getElementById('alerts').innerHTML += alert;
+    }
+
+    var sortedBPs = selectedBPs.sort();
     eos.transaction(tr => {
-        tr.voteproducer("libertylion1", "", ["block21genic", "libertylion1"]);
+        tr.voteproducer("libertylion1", "", sortedBPs);
+    }).then(tx => {
+        var alert = `<div class="alert alert-success" role="alert">
+            Your vote has been cast. Refresh page for new vote counts.<br>
+            TxID: ${tx.transaction_id}
+        </div>`
+        document.getElementById('alerts').innerHTML += alert;
+        document.getElementById('private-key').value = "";
     });
 }
 
+function sendAlert (message, type) {
+    var alert = `<div class="alert alert-warning" role="alert"></div>`
+}
